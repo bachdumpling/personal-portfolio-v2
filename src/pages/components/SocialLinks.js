ad0bef6b4e04994c0a4649af82af613489860f55
Bach Le <100985625+bachdumpling@users.noreply.github.com>
2023-12-29 21:03:08 -0500

    Fix import typo for GitHubIcon

--- src/pages/components/SocialLinks.js
+++ src/pages/components/SocialLinks.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useTheme } from "../../../lib/themeContext";
 import LinkedInIcon from "./icons/LinkedInIcon";
 import DocumentIcon from "./icons/DocumentIcon";
-import GitHubIcon from "./icons/GithubIcon";
+import GithubIcon from "./icons/GithubIcon";
 
 function SocialLinks() {
   const { theme } = useTheme();
@@ -11,7 +11,7 @@ function SocialLinks() {
   return (
     <div className="flex flex-row space-x-4">
       <a href="https://github.com/bachdumpling">
-        <GitHubIcon className="w-5 h-5" fill={fillColor} />
+        <GithubIcon className="w-5 h-5" fill={fillColor} />
       </a>
       <a href="https://www.linkedin.com/in/bachhoangle/">
         <LinkedInIcon className="w-5 h-5" fill={fillColor} />
