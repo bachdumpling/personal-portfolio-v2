b58ce81c8cb78aa4b1c28773b466bcfaa18d7ba4
Bach Le <100985625+bachdumpling@users.noreply.github.com>
2024-01-25 18:07:29 -0500

    Add dynamic social links to SocialLinks component

--- src/pages/components/SocialLinks.js
+++ src/pages/components/SocialLinks.js
@@ -1,25 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTheme } from "../../../lib/themeContext";
 import LinkedInIcon from "./icons/LinkedInIcon.jsx";
 import DocumentIcon from "./icons/DocumentIcon.jsx";
 import GithubIcon from "./icons/GithubIcon.jsx";
 import Link from "next/link";
+import { client } from "../../../lib/sanity.client";
+
+async function fetchSocialLinks() {
+  const query = '*[_type == "socialLinks"]';
+  const data = await client.fetch(query);
+  return data[0]; // Assuming there's only one document for social links
+}
 
 function SocialLinks() {
   const { theme } = useTheme();
-  const fillColor = theme === "dark" ? "#FFFFFF" : "#000000"; // Change as per your color needs
+  const fillColor = theme === "dark" ? "#FFFFFF" : "#000000";
+
+  const [socialLinks, setSocialLinks] = useState(null);
+
+  useEffect(() => {
+    const fetchLinks = async () => {
+      const links = await fetchSocialLinks();
+      setSocialLinks(links);
+    };
+
+    fetchLinks();
+  }, []);
+
+  if (!socialLinks) return null;
+
+  console.log(socialLinks);
 
   return (
     <div className="flex flex-row space-x-4">
-      <Link href="https://github.com/bachdumpling">
-        <GithubIcon className="w-6 h-6" fill={fillColor} />
-      </Link>
-      <Link href="https://www.linkedin.com/in/bachhoangle/">
-        <LinkedInIcon className="w-6 h-6" fill={fillColor} />
-      </Link>
-      <Link href="https://drive.google.com/file/d/10nnloQtrAujyDqxdxJYaIkeS4R7Uaq3p/view?usp=sharing">
-        <DocumentIcon className="w-6 h-6" fill={fillColor} />
-      </Link>
+      {socialLinks.github && (
+        <Link target="_blank" href={socialLinks.github}>
+          <GithubIcon className="w-6 h-6" fill={fillColor} />
+        </Link>
+      )}
+      {socialLinks.linkedin && (
+        <Link target="_blank" href={socialLinks.linkedin}>
+          <LinkedInIcon className="w-6 h-6" fill={fillColor} />
+        </Link>
+      )}
+      {socialLinks.resume && (
+        <Link target="_blank" href={socialLinks.resume}>
+          <DocumentIcon className="w-6 h-6" fill={fillColor} />
+        </Link>
+      )}
     </div>
   );
 }
